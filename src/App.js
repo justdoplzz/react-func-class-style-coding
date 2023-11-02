0e98e061b6d1b1375fd36772b9f9f36a34358dfc
khs <khs1305@dreamsecurity.com>
2023-11-02 10:30:00 +0900

    [class,func] 숫자 불러오기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React, { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello, World!
+    <div className="container">
+      <h1>Hello, World!!!!!</h1>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
+}
+
+function FuncComp(props){
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {props.initNumber}</p>
+    </div>
+  );
+}
+
+
+class ClassComp extends Component{
+  state={
+    number:this.props.initNumber
+  }
+  render(){
+    return(
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
