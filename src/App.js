780b800bc15bc0eedb4d078957dc0f126071ea11
khs <khs1305@dreamsecurity.com>
2023-11-02 11:39:52 +0900

    [class,func] 여러 개의 state 사용법 - class : 하나의 객체 안에서 사용 - function : 각각의 useState 만들어서 사용

--- src/App.js
+++ src/App.js
@@ -16,13 +16,29 @@ function FuncComp(props){
   var number = numberState[0];                      // 첫번째 값은 현재 상태값임
   var setNumber = numberState[1];                   // 두번째 값은 상태를 바꿀수있는 값임(=setState)
   console.log('numberState', numberState);
+
+  // 1. useState 기본 사용법
+  // var dateState = useState((new Date()).toString());
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+
+  // 2. 축약된 useState 사용법
+  var [_date, setDate] = useState((new Date()).toString());
+
   return (
     <div className="container">
       <h2>function style component</h2>
+      
       <p>Number : {number}</p>
       <input type="button" value="random" onClick={
           function(){
             setNumber(Math.random());
+          }}></input>
+      
+      <p>Date : {_date}</p>
+      <input type="button" value="date" onClick={
+          function(){
+            setDate((new Date()).toString());
           }}></input>
     </div>
   );
@@ -30,16 +46,24 @@ function FuncComp(props){
 
 class ClassComp extends Component{
   state={
-    number:this.props.initNumber            // props 를 통해 전달된 값을 set 함
+    number:this.props.initNumber,            // props 를 통해 전달된 값을 set 함
+    date:(new Date()).toString()
   }
   render(){
     return(
       <div className="container">
         <h2>class style component</h2>
+
         <p>Number : {this.state.number}</p>
         <input type="button" value="random" onClick={
           function(){
             this.setState({number:Math.random()})
+          }.bind(this)}></input>
+
+        <p>Date : {this.state.date}</p>
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date:(new Date()).toString()})
           }.bind(this)}></input>
       </div>
     )
